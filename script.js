af5a8ffcb2615b92942ea2d6ee73785b97c9a11d
Liso16 <146746900+Liso16@users.noreply.github.com>
2026-04-10 19:13:01 +0100

    structure progress of web app

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function showSection(sectionId) {
     });
     document.getElementById(sectionId).style.display ="block";
 }
+
+function openTab(tabId) {
+    
+    const contents = document.querySelectorAll('.tab-content');
+    contents.forEach(c => c.style.display = 'none');
+
+   
+    const buttons = document.querySelectorAll('.tab-btn');
+    buttons.forEach(b => b.classList.remove('active'));
+
+    
+    document.getElementById(tabId).style.display = 'block';
+
+
+    event.target.classList.add('active');
+}
